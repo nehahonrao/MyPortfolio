4092c2f0f52bb254a0eeb0f2fe113c46a5a5b44f
unknown <nehalonkar27@gmail.com>
2022-02-11 15:53:51 +0100

    working on portfolio

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,7 +1,17 @@
 import './about.scss';
+import { init } from 'ityped';
+import { useEffect,useRef } from 'react';
 
 
 export default function About() {
+ const textref = useRef();
+    useEffect(()=>{
+        init(textref.current, 
+        {showCursor: true, 
+            backDelay:  1500,
+        strings: ['FrontEnd Web Developer','UI Designer'],
+     });
+    },[])
     return (
         <div className='about' id="about">
             <div className="left">
@@ -9,8 +19,14 @@ export default function About() {
                  <img src="image/neha.jpg" alt="" />
              </div>
             </div>
-            <div className="right"></div>
-            
+            <div className="right">
+            <div className="wrapper">
+          <h2>Hi There, I am</h2>
+          <h1>Neha Mayur Honrao</h1>
+          <h3 className="heading">.<span ref={textref}></span></h3>
+          
+        </div>
+        </div>
         </div>
     )
 }
--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,31 +1,29 @@
-import './topbar.scss';
-import React from 'react';
-import {Person,Mail} from "@material-ui/icons";
+import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
+export default function Topbar({ menuOpen , setMenuOpen }) {
+    return (
+        <div className = {"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                <a href="#intro" className = "logo">Zain Imtiaz</a>
+             <div className="itemContainer">
+                     <Person className = "icon" />
+                     <span>+32 466 08 60 11</span>
+                       </div> 
+                     <div className="itemContainer">
+                     <Mail className="icon"/>
+                     <span>zenimtiazz@gmail.com</span>
+                     </div>   
+                </div>
+                <div className="right">
+                <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                <span className="line1"></span>
+               <span className="line2"></span>
+               <span className="line3"></span>
 
-export default function Topbar({menuOpen,setMenuOpen}) {
-return (
-<div className={'topbar ' + (menuOpen && "active")} id="topbar">
-    <div className="wrapper">
-        <div className="left">
-            <a href="#about" className='logo'>Neha Mayur Honrao</a>
-            <div className="itemContainer">
-                <Person className="icon"/>
-                <span>+32 465684998</span>
-            </div>
-            <div className="itemContainer">
-                <Mail className="icon" />
-                <span>nehalonkar27@gmail.com</span>
+                </div>
+                </div>
             </div>
         </div>
-        
-        <div className="right">
-            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-                <span className='line'></span>
-                <span className='line1'></span>
-                <span className='line2'></span>
-            </div>
-        </div>
-    </div>
-</div>
-)
+    );
 }
